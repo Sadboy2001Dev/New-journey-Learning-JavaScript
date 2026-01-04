683e5c3f22bec10b4270e631b03c89f5ec12ea0f
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-01-04 21:17:04 +0100

    file variable, declared

---
+++ chapter_2/variable.js
@@ -0,0 +1,26 @@
+/*
+ 	VARIABLES
+*/
+
+let	number = 12;
+console.log (number);
+
+number = 23;
+console.log (number);
+
+var	age = 30;
+console.log (age);
+
+age = 45;
+console.log (age);
+
+let	names = new Array ();
+names = ["Gil", "Kupessa", "Cruz", "Carlos"];
+console.log (names);
+
+let	Person = {
+	name: 'Gil',
+	surname: 'Kupessa',
+	status: 'online'
+};
+console.log (Person)
