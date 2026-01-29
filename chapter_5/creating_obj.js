c9399520f886cc415bc132f3f578ae73c20de7af
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-01-29 22:23:44 +0100

    File Creating object, with examples of how to create a object

---
+++ chapter_5/creating_obj.js
@@ -0,0 +1,9 @@
+// Creating Object
+
+// Object.create ()
+let o = Object.create ({name:"Kupessa", age: 57});
+console.log (o);
+
+// new Object ()
+let ob = new Object (); // an empty object created
+console.log (ob);
