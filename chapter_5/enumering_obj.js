1d20f99c740a34882e864efe2ebfc3f70059a36b
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-02-02 21:55:51 +0100

    Enumerable properties

--- chapter_5/enumering_obj.js
+++ chapter_5/enumering_obj.js
@@ -5,4 +5,4 @@ let obj = { x:1, y:2, z:3 }; // enumerated obj
 console.log (obj.propertyIsEnumerable ("toString")); //false
 
 for (let p in obj) // interating with all properties
-	console.log (p); // x, y, z
+	console.log (p); // x, y, z - properties
