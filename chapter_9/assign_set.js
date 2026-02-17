594e8fb8d6c21b243c459276bd654be09ebaa81c
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-02-18 00:36:22 +0100

    Assign values to Sets

---
+++ chapter_9/assign_set.js
@@ -0,0 +1,10 @@
+// created Set
+let roles = new Set ();
+
+// assign values to Set
+// single value
+roles.add ("User"); // added
+roles.add ("User"); // wasn't added
+roles.add ("Admin"); // added
+
+console.log (roles); // [User, Admin]
