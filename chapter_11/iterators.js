1f5c869e766ab8661e174dc55b8eb1dd56a60a41
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-02-24 14:25:42 +0100

    First examples of interators and introducing next and values methods

---
+++ chapter_11/iterators.js
@@ -0,0 +1,26 @@
+// array called book created
+
+const book = [
+"Twinkle, twinkle, little bat!",
+ "How I wonder what you're at!",
+ "Up above the world you fly,",
+ "Like a tea tray in the sky.",
+ "Twinkle, twinkle, little bat!",
+ "How I wonder what you're at!",
+];
+
+// iterator (represented by letter it)
+// receive the values of the array book
+// to receive the values it's used the method "values ()"
+const it = book.values ();
+
+// reading the the pages of the book
+// to read is used the method "next ()"
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
+console.log (it.next ());
