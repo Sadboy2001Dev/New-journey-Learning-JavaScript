e46d18a9625f959f511d9903f70c6b98c3fbc8ee
Hermenegildo Kupessa <kupessagil19@gmail.com>
2026-02-24 21:28:42 +0100

    Iterators file updated

--- chapter_11/iterators.js
+++ chapter_11/iterators.js
@@ -38,3 +38,32 @@ console.log (it.next ());
  * same thing. Once an iterator is done, it’s done, and it shouldn’t ever 
  * go back to providing you data.
  */
+
+/*
+ * This way it won't print the unexpected values (undefined)
+ */
+const It = book.values ();
+let current = It.next ();
+
+while (!current.done) {
+	console.log (current.value);
+	current = It.next ();
+}
+
+/*
+ * iterators are distinct; that is, every time you create a new iterator,
+ * you’re starting at the beginning, and it’s possible to have multiple
+ * iterators that are at different places.
+ */
+const it1 = book.values ();
+const it2 = book.values ();
+
+// read two pages with it1:
+console.log (it1.next());
+console.log (it1.next());
+
+// read one page with it2:
+console.log (it2.next());
+
+// read third page with it1:
+console.log (it1.next());
